a5ad559979da30180327fb507a3aa42834f27ea0
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-20 20:50:53 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,29 @@
+// import React from "react";
+
+
+// function Dashboard({ setLoggedIn }) {
+
+//     return (
+//         <div id="dashboardPage">
+//             <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
+//             <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
+//         </div>
+//     )
+
+// }
+// export default Dashboard;
+
 import React from "react";
 
+const Dashboard = ({ handleLogout }) => {
+return (
+<div id="dashboardPage">
+<h1 id="dashboard-tag">Welcome to Dashboard</h1>
+<button id="dashboard-logout" onClick={handleLogout}>
+Log out
+</button>
+</div>
+);
+};
 
-function Dashboard({ setLoggedIn }) {
-
-    return (
-        <div id="dashboardPage">
-            <h1 id="dashboard-tag"> Welcome to Dashboard</h1>
-            <button id="dashboard-logout" onClick={() => setLoggedIn(false)}>Log out </button>
-        </div>
-    )
-
-}
 export default Dashboard;
